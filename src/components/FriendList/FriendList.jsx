82c04f9dc285be267e953937e19c3a646931f5e6
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2023-01-20 17:26:57 +0200

    NEW

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './friendList.module.css';
+import FriendListItem from './FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => (
   <ul className={styles.friend_list}>
